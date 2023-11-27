9814c66e1990f43348f763744e4b18306163d1a9
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-11-27 22:04:18 +0200

    updated api

--- src/components/hero/Recommand.jsx
+++ src/components/hero/Recommand.jsx
@@ -1,19 +1,51 @@
-import React from "react"
-import { recommand, treading } from "../assets/data/data"
-import { Card_sm } from "../common/Card_sm"
-import { Title } from "../common/Title"
+// Recommand.js
+
+import React, { useEffect, useState } from "react";
+import { recommand } from "../assets/data/data";
+import { Card_sm } from "../common/Card_sm";
+import { Title } from "../common/Title";
+import { fetchData } from "../../utils/api";
 
 export const Recommand = () => {
-  return (
-    <>
-      <section className='treading hero mt-7 pb-32'>
-        <Title title='Recommand for you' />
-        <div className='grid grid-cols-2 gap-5'>
-          {recommand.map((item, i) => (
-            <Card_sm cover={item.cover} name={item.name} tag={item.tag} i={i} />
-          ))}
-        </div>
-      </section>
-    </>
-  )
-}
+   const [data, setData] = useState([]);
+   const [loading, setLoading] = useState(true);
+
+   useEffect(() => {
+      const fetchDataAsync = async () => {
+         try {
+            const fetchedData = await fetchData("artists/");
+            setData(fetchedData);
+         } catch (error) {
+            // Handle the error if needed
+            console.error(error.message);
+         } finally {
+            setLoading(false);
+         }
+      };
+
+      fetchDataAsync();
+   }, []);
+   console.log(data);
+   return (
+      <>
+         <section className='treading hero mt-7 pb-32'>
+            <Title title='Recommand for you' />
+            {loading ? (
+               <p>Loading...</p>
+            ) : (
+               <div className='grid grid-cols-2 gap-5'>
+                  {data?.length > 0 &&
+                     data.map((item) => (
+                        <Card_sm
+                           cover={item.images}
+                           name={item.name}
+                           tag={item.genres}
+                           key={item.id}
+                        />
+                     ))}
+               </div>
+            )}
+         </section>
+      </>
+   );
+};
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,26 @@
-import axios from 'axios'
+// api.js
 
-const options = {
-  method: 'GET',
-  url: 'https://spotify23.p.rapidapi.com',
-  params: {
-    id: '55EWbmkVr8iDNmSyperh9o'
-  },
-  headers: {
-    'X-RapidAPI-Key': '1ec3413504msh1dfcafc2c42e638p16453cjsn90e6b472ab25',
-    'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-  }
+import axios from "axios";
+
+export const fetchData = async (endpoint) => {
+   const options = {
+      method: "GET",
+      url: `https://spotify23.p.rapidapi.com/${endpoint}`,
+      params: {
+         ids: "2w9zwq3AktTeYYMuhMjju8",
+      },
+      headers: {
+         "X-RapidAPI-Key": "1ec3413504msh1dfcafc2c42e638p16453cjsn90e6b472ab25",
+         "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
+      },
+   };
+
+   try {
+      const response = await axios.request(options);
+      return response.data; // Return the data instead of logging it
+      console.log(response);
+   } catch (error) {
+      console.error(error);
+      throw new Error("Failed to fetch data");
+   }
 };
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
